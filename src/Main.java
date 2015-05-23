94436c9a46d52480b190a57c7c540d3da3ba6a4a
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-05-24 02:07:03 +0800

    24/5/2015 2:06AM
    Creating a thread for listening port.
    Tested receive data from phone.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {  
   public static void main(String[] args) {
     new TicTacToeServer();
--- src/TicTacToeServer.java
+++ src/TicTacToeServer.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -6,23 +8,42 @@ import java.util.logging.Logger;
 
 public class TicTacToeServer {
   
-  final int DEFAULT_PORT = 6666;
+  final int LISTENING_PORT = 6666;
   
   ServerSocket TicTacToeServerSocket;
   
   public TicTacToeServer() {
-    try {
-      TicTacToeServerSocket = new ServerSocket(DEFAULT_PORT);
-      
-      while (true) {
-        System.out.println("Waiting connection....");
+    new ListeningPort().start();
+  }
+  
+  private class ListeningPort extends Thread {
+    
+    @Override
+    public void run() {
+      try {
+        TicTacToeServerSocket = new ServerSocket(LISTENING_PORT);
+        while (true) {
+          System.out.println("Waiting connection....");
         
-        Socket clientSocket = TicTacToeServerSocket.accept();
-        System.out.println("Connection from: " + clientSocket);
+          Socket clientSocket = TicTacToeServerSocket.accept();
+          System.out.println("Connection from: " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort());
+        }
+      } catch (IOException ex) {
+        Logger.getLogger(TicTacToeServer.class.getName()).log(Level.SEVERE, null, ex);
       }
-    } 
-    catch (IOException ex) {
-      Logger.getLogger(TicTacToeServer.class.getName()).log(Level.SEVERE, null, ex);
     }
+  }
+  
+  private void ReceiveBoardStatus(Socket ClientSocket) {
+    
+    /*ObjectInputStream in = new ObjectInputStream(ClientSocket.getInputStream());
+    char[][] bb = (char[][])in.readObject();
+        
+    for (int a = 0; a < 3; a++) {
+      for (int b = 0; b < 3; b++) {
+        System.out.print(bb[a][b] + " ");
+      }
+      System.out.println("");
+    }*/
   }
 }
