3d13b3b4245af24f3ca383ba35c76f305967fd27
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-05-25 00:10:31 +0800

    .

--- src/GameThread.java
+++ src/GameThread.java
@@ -7,6 +7,9 @@ public class GameThread extends Thread{
   public GameThread(Socket Player1, Socket Player2) {
     this.Player1 = Player1;
     this.Player2 = Player2;
+    System.out.println("GameThread Started!");
+    System.out.println(Player1);
+    System.out.println(Player2);
   }
   
 }
