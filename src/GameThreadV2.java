d3bdf1b187ec2769e1e43cccbca2a0bf3aa916cd
Ling Tze Hock <thock900805@gmail.com>
2015-05-26 17:01:21 +0800

    Create GameThreadV2.java

---
+++ src/GameThreadV2.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+
+public class GameThread extends Thread{
+  
+  Socket Player1, Player2;
+  
+  public GameThread(Socket Player1, Socket Player2) {
+    this.Player1 = Player1;
+    this.Player2 = Player2;
+    System.out.println("GameThread Started!");
+    System.out.println(Player1);
+    System.out.println(Player2);
+   
+    ReceiveBoardStatus(this.Player1);
+  }
+  
+  private void ReceiveBoardStatus(Socket ClientSocket) {
+	  
+		try {
+		  ByteBuffer bbb = ByteBuffer.allocate(100);
+		  InputStream in = ClientSocket.getInputStream();
+		  in.read(bbb.array());
+		  String message = new String(bbb.array()).trim();
+		  System.out.println("mes is " + message);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+  }
+}
