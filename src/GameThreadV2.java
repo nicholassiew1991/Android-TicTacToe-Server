f06ddf4ab2a04038b8924ddf81c633d7b8f1f6a8
Ling Tze Hock <thock900805@gmail.com>
2015-05-27 16:47:48 +0800

    Update GameThreadV2.java

--- src/GameThreadV2.java
+++ src/GameThreadV2.java
@@ -1,35 +1,58 @@
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
-import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class GameThreadV2 extends Thread{
+public class GameThread extends Thread{
   
-  Socket Player1, Player2;
+  private Socket Player1, Player2;
   
-  public GameThreadV2(Socket Player1, Socket Player2) {
+  private static int GameCount = 0;
+  
+  public GameThread(Socket Player1, Socket Player2) {
     this.Player1 = Player1;
     this.Player2 = Player2;
     System.out.println("GameThread Started!");
+    GameThread.GameCount++;
     System.out.println(Player1);
     System.out.println(Player2);
-   
+    
     ReceiveBoardStatus(this.Player1);
   }
   
+  public int GetGameCount() {
+    return GameThread.GameCount;
+  }
+  
+  @Override
+  public void run() {
+    
+    while (true) {
+      
+    }
+  }
+  
   private void ReceiveBoardStatus(Socket ClientSocket) {
-	  
-		try {
-		  ByteBuffer bbb = ByteBuffer.allocate(100);
-		  InputStream in = ClientSocket.getInputStream();
-		  in.read(bbb.array());
-		  String message = new String(bbb.array()).trim();
-		  System.out.println("mes is " + message);
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    
+    while(true){
+    	try {
+    	      ObjectInputStream in = new ObjectInputStream(ClientSocket.getInputStream());
+    	      char[][] bb = (char[][])in.readObject();
+    	      
+    	      for(int i=0;i<3;i++){
+    	      	for(int j=0;j<3;j++){
+    	      		System.out.print(bb[i][j]+" ");
+    	      	}
+    	      	System.out.println();
+    	      }
+    	    } 
+    	    catch (IOException ex) {
+    	      Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
+    	    } 
+    	    catch (ClassNotFoundException ex) {
+    	      Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
+    	    }
+    }
   }
 }
