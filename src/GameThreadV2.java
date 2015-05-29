499e04541ff50a5afbc67367e49ee4570ac889dd
Ling Tze Hock <thock900805@gmail.com>
2015-05-29 10:48:16 +0800

    Update GameThreadV2.java

--- src/GameThreadV2.java
+++ src/GameThreadV2.java
@@ -1,24 +1,28 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.omg.CORBA.portable.OutputStream;
 
 public class GameThread extends Thread{
   
   private Socket Player1, Player2;
   
   private static int GameCount = 0;
-  
+  private char[][] bb;
   public GameThread(Socket Player1, Socket Player2) {
     this.Player1 = Player1;
     this.Player2 = Player2;
+    
     System.out.println("GameThread Started!");
     GameThread.GameCount++;
     System.out.println(Player1);
     System.out.println(Player2);
     
-    ReceiveBoardStatus(this.Player1);
+    
   }
   
   public int GetGameCount() {
@@ -27,10 +31,7 @@ public class GameThread extends Thread{
   
   @Override
   public void run() {
-    
-    while (true) {
-      
-    }
+	  ReceiveBoardStatus(this.Player1);
   }
   
   private void ReceiveBoardStatus(Socket ClientSocket) {
@@ -38,7 +39,7 @@ public class GameThread extends Thread{
     while(true){
     	try {
     	      ObjectInputStream in = new ObjectInputStream(ClientSocket.getInputStream());
-    	      char[][] bb = (char[][])in.readObject();
+    	      bb = (char[][])in.readObject();
     	      
     	      for(int i=0;i<3;i++){
     	      	for(int j=0;j<3;j++){
@@ -46,6 +47,8 @@ public class GameThread extends Thread{
     	      	}
     	      	System.out.println();
     	      }
+    	      
+    	      SendBoardStatus(this.Player2);
     	    } 
     	    catch (IOException ex) {
     	      Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
@@ -54,5 +57,17 @@ public class GameThread extends Thread{
     	      Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
     	    }
     }
+  }
+  
+  private void SendBoardStatus(Socket client){
+	  try {
+	      OutputStream outToClient = (OutputStream) client.getOutputStream();
+	      ObjectOutputStream out = new ObjectOutputStream(outToClient);
+	      out.writeObject(bb);
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }catch( java.lang.ClassCastException e){
+	    	System.out.println("Cast problem");
+	    }
   }
 }
