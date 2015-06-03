18845dfe0c9f4e1effba2a4921fccc108222d2fc
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-06-04 02:19:09 +0800

    Use a new method to send and receive.

--- src/GameThread.java
+++ src/GameThread.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
@@ -11,7 +13,7 @@ public class GameThread {
 
   private Socket Player1, Player2;
 
-  private char[][] GameBoard;
+  private char[][] GameBoard = new char[3][3];
 
   public GameThread(Socket Player1, Socket Player2) {
     this.Player1 = Player1;
@@ -21,10 +23,66 @@ public class GameThread {
   }
   
   private void StartReceiveMessagesThread() {
-    new ReceiveMessagesThread(this.Player1, this.Player2).start();
-    new ReceiveMessagesThread(this.Player2, this.Player1).start();
+    new NewReceiveMessageThread(this.Player1, this.Player2).start();
+    new NewReceiveMessageThread(this.Player2, this.Player1).start();
   }
+  
+  private class NewReceiveMessageThread extends Thread {
+    
+    private Socket SourceClient, DestinationClient;
 
+    NewReceiveMessageThread(Socket SourceClient, Socket DestinationClient) {
+      this.SourceClient = SourceClient;
+      this.DestinationClient = DestinationClient;
+    }
+    
+    @Override
+    public void run() {
+      while (true) {
+        try {
+          DataInputStream dIn = new DataInputStream(SourceClient.getInputStream());
+          byte MessagesType = dIn.readByte();
+          
+          if (MessagesType == ServerGlobal.BOARD_STATUS) {
+            
+            System.out.println("Receive BoardStatus from: " + this.SourceClient);
+            
+            for (int a = 0; a < 3; a++) {
+              for (int b = 0; b < 3; b++) {
+                GameBoard[a][b] = dIn.readChar();
+                System.out.print(GameBoard[a][b] + " ");
+              }
+              System.out.println("");
+            }
+            
+            NewSendBoardStatus(this.DestinationClient);
+          }
+        }
+        catch (IOException ex) {
+          Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
+    }
+    
+    private void NewSendBoardStatus(Socket DestClient) {
+      try {
+        char[][] BoardStatus = GameBoard;
+        DataOutputStream dOut = new DataOutputStream(DestClient.getOutputStream());
+        dOut.writeByte(ServerGlobal.BOARD_STATUS);
+        for (int a = 0; a < 3; a++) {
+          for (int b = 0; b < 3; b++) {
+            dOut.writeChar(BoardStatus[a][b]);
+          }
+        }
+        dOut.flush(); // Send out
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+  
+  //<editor-fold defaultstate="collapsed" desc="Not use temporary">
   private void SendBoardStatus(Socket DestClient) {
     try {
       OutputStream outToClient = DestClient.getOutputStream();
@@ -54,22 +112,16 @@ public class GameThread {
       while (true) {
         try {
           ObjectInputStream in = new ObjectInputStream(this.SourceClient.getInputStream());
-
-          switch (in.readByte()) {
-            case ServerGlobal.BOARD_STATUS:
-              GameBoard = (char[][]) in.readObject();
-              SendBoardStatus(this.DestinationClient);
-              break;
-          }
         }
         catch (java.io.StreamCorruptedException ex) {
           ex.printStackTrace();
           break;
         }
-        catch (IOException | ClassNotFoundException ex) {
-          Logger.getLogger(GameThread.class.getName()).log(Level.SEVERE, null, ex);
+        catch (IOException ex) {
+         ex.printStackTrace();
         }
       }
     }
   }
+  //</editor-fold>
 }
