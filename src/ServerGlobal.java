9965f3753e939780c97d41f34754e5c0d78aa95b
Nicholas Siew <nicholas_siew1991@hotmail.com>
2015-06-05 17:23:00 +0800

    5/6/2015 5:22 PM

--- src/ServerGlobal.java
+++ src/ServerGlobal.java
@@ -1,7 +1,8 @@
 public class ServerGlobal {
   
-  /** Message Type Constant Declaration **/
-  static final byte CLIENT_CONNECTED = 1;
-  static final byte BOARD_STATUS = 2;
-  static final byte GET_PLAYER_SYMBOL = 3;
+  /* Message Type Constant Declaration */
+  static final byte CLIENT_WAIT_PAIRING = 1;
+  static final byte SET_PLAYER_SYMBOL = 2;
+  static final byte BOARD_STATUS = 3;
+  static final byte GET_PLAYER_SYMBOL = 4;
 }
--- src/TicTacToeServer.java
+++ src/TicTacToeServer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
@@ -15,11 +16,11 @@ public class TicTacToeServer {
   ServerSocket TicTacToeServerSocket;
   
   List ClientList = new ArrayList();
+  List WiatForPairingList = new ArrayList();
   
   public TicTacToeServer() {
     new ListeningPort().start();
-    //new PrintClientList().start();
-    new PairingPlayer().start();
+    new NewPairingPlayer().start();
   }
   
   private class ListeningPort extends Thread {
@@ -33,6 +34,7 @@ public class TicTacToeServer {
         
           Socket clientSocket = TicTacToeServerSocket.accept();
           System.out.println("Connection from: " + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort());
+          new WaitPairingResponse(clientSocket).start();
           ClientList.add(clientSocket);
         }
       } 
@@ -42,60 +44,76 @@ public class TicTacToeServer {
     }
   }
   
-  private class PairingPlayer extends Thread {
+  private class WaitPairingResponse extends Thread {
+    
+    private Socket Client;
+    private boolean IsDone = false;
+    
+    public WaitPairingResponse(Socket Client) {
+      this.Client = Client;
+    }
+    
+    private void Done() {
+      IsDone = true;
+    }
+    
+    @Override
+    public void run() {
+      
+      while (this.IsDone == false) {
+        try {
+          System.out.println("Waiting pairing message from " + this.Client);
+          ObjectInputStream in = new ObjectInputStream(this.Client.getInputStream());
+          
+          if (in.readByte() == ServerGlobal.CLIENT_WAIT_PAIRING) {
+            WiatForPairingList.add(this.Client);
+            System.out.println(this.Client + " is wait for pairing...");
+            Done();
+          }
+        }
+        catch (IOException ex) {
+          ex.printStackTrace();
+        }
+      }
+    } 
+  }
+  
+  private class NewPairingPlayer extends Thread {
     
     public void run() {
+      
       while (true) {
-        
         try {
           Thread.sleep(1000);
         } 
         catch (InterruptedException ex) {
-          Logger.getLogger(TicTacToeServer.class.getName()).log(Level.SEVERE, null, ex);
+          ex.printStackTrace();
         }
         
-        if (ClientList.size() >= 2) {
-          Socket Player1 = ((Socket)ClientList.get(0));
-          Socket Player2 = ((Socket)ClientList.get(1));
+        if (WiatForPairingList.size() >= 2) {
+          Socket Player1 = ((Socket)WiatForPairingList.get(0));
+          Socket Player2 = ((Socket)WiatForPairingList.get(1));
           
-          if (isClientConnecting(Player1) == false) {
-            ClientList.remove(0);
-          }
-          else if (isClientConnecting(Player2) == false) {
-            ClientList.remove(1);
-          }
-          else {
-            new GameThread(Player1, Player2);
-            ClientList.remove(1);
-            ClientList.remove(0);
-          }
+          SendPlayerSymbol(Player1, 'O');
+          SendPlayerSymbol(Player2, 'X');
+          
+          new GameThread(Player1, Player2);
+          
+          WiatForPairingList.remove(1);
+          WiatForPairingList.remove(0);
         }
       }
     }
     
-    private boolean isClientConnecting(Socket Client) {
+    private void SendPlayerSymbol(Socket Player, char Symbol) { 
       try {
-        Client.sendUrgentData(0xFF);
-        return true;
-      } 
-      catch (IOException ex) {
-        return false;
+        OutputStream ToPlayerOne = Player.getOutputStream();
+        ObjectOutputStream OutToPlayer1 = new ObjectOutputStream(ToPlayerOne);
+        OutToPlayer1.writeByte(ServerGlobal.SET_PLAYER_SYMBOL);
+        OutToPlayer1.writeObject(Symbol);
       }
-    }
-  }
-  
-  private class PrintClientList extends Thread {
-    
-    public void run() {
-      while (true) {
-        System.out.println(ClientList);
-        
-        try {
-          Thread.sleep(5000);
-        } 
-        catch (InterruptedException ex) {
-          Logger.getLogger(TicTacToeServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
+      catch (IOException e) {
+        e.printStackTrace();
       }
     }
   }
